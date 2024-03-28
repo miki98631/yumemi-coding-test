f0d5a6eaa4bd3bbe5212190cb746daf0b3030845
miki98631 <mikinatsutomo@icloud.com>
2024-03-28 15:59:36 +0900

    mod

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: '/<REPO>/',
+  base: '/yumemi-coding-test/',
   test: {
     globals: true,
     environment: 'happy-dom',
